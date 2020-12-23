ddf5158b8ccbc825b874aee2f41017b6b870fd2d
abhishekteakade <abhishektekade45@gmail.com>
2020-12-23 11:42:27 +0530

    second commit

--- setup-commands.sh
+++ setup-commands.sh
@@ -2,4 +2,6 @@ sudo apt install fortune
 sudo apt install cowsay
 sudo apt install git
 sudo apt install vcprompt
-sudo apt install firefox    
+sudo apt install firefox  
+sudo apt install nvm
+nvm install stable  
